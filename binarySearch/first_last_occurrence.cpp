c5cb4cd46450089c8a16de83130c1ed1cd334192
Daya <156498789+Richie029@users.noreply.github.com>
2024-02-13 10:51:45 +0530

    Create first_last_occurrence.cpp

---
+++ binarySearch/first_last_occurrence.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+int firstOcc(int arr[],int n,int key){
+    int s=0,e=n-1;
+    int mid=s+((e-s)/2);
+    int ans=-1;
+    while(s<=e){
+        if(arr[mid]==key){
+            ans=mid;
+            e=mid-1;
+        } else if(arr[mid]<key){
+            s=mid+1;
+        } else{
+            e=mid-1;
+        }
+        mid = s +( (e - s) / 2);
+    }
+    return ans;
+}
+int lastOcc(int arr[],int n,int key){
+    int s=0,e=n-1;
+    int mid=s+((e-s)/2);
+    int ans=-1;
+    while(s<=e){
+        if(arr[mid]==key){
+            ans=mid;
+            s=mid+1;
+        }else if(key>arr[mid]){
+            s=mid+1;
+        } else{
+            e=mid-1;
+        }
+        mid = s+((e-s)/2);
+    }
+    return ans;
+}
+int main(){
+    int arr[5]={3,3,3,3,3};
+    cout<<"First Occurence is : "<<firstOcc(arr,5,3)<<endl;
+    cout<<"Last Occurence is : "<<lastOcc(arr,5,3)<<endl;
+
+    return 0;
+
+}
+
+
+
+// total number of occurence =  (last index - first index )+ 1 
