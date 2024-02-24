f63c0c5b54b83c1886cbdcb22d5e1e923fabeeef
Daya <156498789+Richie029@users.noreply.github.com>
2024-02-24 22:33:25 +0530

    Create segregate_0_1.cpp

---
+++ Array/segregate_0_1.cpp
@@ -0,0 +1,21 @@
+// 1 0 1 0 1 0  ===>  0 0 0 1 1 1
+
+
+
+void segregate0and1(int arr[], int n) {
+        int start=0;
+        int end=n-1;
+        while(start<=end){
+            if(arr[start]==1 && arr[end]==0){
+                swap(arr[start],arr[end]);                
+            } 
+            if(arr[start]==0){
+                start++;
+            }
+            if(arr[end]==1){
+                end--;
+            }
+        
+        }
+        
+    }
