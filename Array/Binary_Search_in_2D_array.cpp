7a92b7a8e6c8e5e574f1bcad9fba9973eb0469f8
Daya <156498789+Richie029@users.noreply.github.com>
2024-03-24 10:02:57 +0530

    Create Binary_Search_in_2D_array.cpp

---
+++ Array/Binary_Search_in_2D_array.cpp
@@ -0,0 +1,18 @@
+ bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int N=matrix.size(),M=matrix[0].size();
+        int start=0,end=(N*M)-1;
+    while (start<=end) {
+        int mid=start+((end-start)/2);
+        int row_idx=mid/M;
+        int col_idx=mid%M;
+        if(matrix[row_idx][col_idx]==target){
+            return true;
+        }
+        else if (matrix[row_idx][col_idx]<target){
+            start=mid+1;
+        } else{
+            end=mid-1;
+        }
+    }
+    return false;
+    }
